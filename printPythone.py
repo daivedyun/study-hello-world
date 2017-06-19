ee762e349880c24d2bb2aaf5925e3178a3752970
ydy <Daiyun.Yang@windriver.com>
2017-06-19 15:41:50 -0600

    my frist pythone3!!

---
+++ printPythone.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+
+list = ["sddfsd",1000,2.3333,"jjjsdkj",333.332]
+tinylist = ["ilove","china"]
+a,b,c = 1,2.5,"sdfsdfjsldjfslj" 
+counter = 100 
+miles = 1000.0 
+name = "John" # ???
+
+tinylist[1]= "china too"
+dict = {};
+dict['one'] = "this is one"
+dict[2] = "this is two!!!!!!"
+
+tinydict = {'name':'edward', 'employnumber':655589, 'dept':'sale'}
+
+print dict['one']
+print dict[2]
+print tinydict
+print tinydict.keys()
+print tinydict.values()
+print tinydict['name']
+print '\n'
+
+print counter
+print miles
+print name
+print a
+print b
+print c[2:7]
+print c*5
+print c+"TEST"
+print list[0]
+print list[2:4]
+print tinylist *2
+print list + tinylist
+
+
+
