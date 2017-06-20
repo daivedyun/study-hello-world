5aaa279e1620449274135055ae1dd6f219d7a158
ydy <Daiyun.Yang@windriver.com>
2017-06-20 11:19:09 -0600

    changeTheDateType for pythone study!!!

---
+++ study_testmatrices/changeTheDateType.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+
+a = 10
+b = 11.2
+st = "git hub is good stools for coding!!!"
+
+print type(a)
+print type(b)
+print type(st)
+print isinstance(a,int)
+
+print int(b)
+
+print b
+print type(b)
