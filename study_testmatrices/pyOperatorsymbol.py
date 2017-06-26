518097e5bd50fcf9d2c45e32531e3fa74d12f9ff
ydy <Daiyun.Yang@windriver.com>
2017-06-26 14:33:20 -0600

    pythone operatol symbol!!!!

---
+++ study_testmatrices/pyOperatorsymbol.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+a = 21
+b = 10
+c =0
+
+c = a + b
+print "1-c the value is ",c
+c = a/b 
+print "2-c a/b the value is ",c
+
+d = 1/2
+print "1-d a/b the value is ",d
+
+f = 1.0/2
+print "1-f a/b the value is ",f
+
+if (a==b):
+  print "a =b  "
+else:
+  print "a!=b"
+
+if (b>c):
+  print "a is real bigger than c"
+else:
+  print "c<b"
+
+
+
