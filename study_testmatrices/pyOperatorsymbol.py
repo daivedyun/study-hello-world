62fd641d02a15fb1081c1f30fa30a456406453b5
ydy <Daiyun.Yang@windriver.com>
2017-06-26 17:23:18 -0600

    jsk

--- study_testmatrices/pyOperatorsymbol.py
+++ study_testmatrices/pyOperatorsymbol.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
 #http://www.360doc.com/content/17/0406/07/5633793_643233897.shtml vim config
+#
 
 a = 21
 b = 10
@@ -26,6 +27,36 @@ if (b>c):
   print "a is real bigger than c"
 else:
   print "c<b"
+if (b!=c):
+  print "b is not equal to c"
+else:
+  print "b is greater than c"
 
 
+
+
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+ 
+a = 60            # 60 = 0011 1100 
+b = 13            # 13 = 0000 1101 
+c = 0
+ 
+c = a & b;        # 12 = 0000 1100
+print "1 - c ???:", c
+ 
+c = a | b;        # 61 = 0011 1101 
+print "2 - c ???:", c
+ 
+c = a ^ b;        # 49 = 0011 0001
+print "3 - c ???:", c
+ 
+c = ~a;           # -61 = 1100 0011
+print "4 - c ???:", c
+ 
+c = a << 2;       # 240 = 1111 0000
+print "5 - c ???:", c
+ 
+c = a >> 2;       # 15 = 0000 1111
+print "6 - c ???:", c
 
